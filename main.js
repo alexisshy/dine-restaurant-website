e4146baa29cf1fe59f90fb4e8c24f6ca85d4fad2
alexisshy <30672309+alexisshy@users.noreply.github.com>
2022-05-24 13:10:08 -0400

    add js to events section

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function () {
-    const button_family=document.getElementById('button-family');
-    const button_special=document.getElementById('button-special');
-
-    const events_image=document.getElementById('events-image');
+    const button_family = document.getElementById('button-family');
+    const button_special = document.getElementById('button-special');
+    const button_social = document.getElementById('button-social');
+    const events_image = document.getElementById('events-image');
 
     button_family.onclick=function () {
         events_image.setAttribute('src', './images/homepage/family-gathering-desktop.jpg');
@@ -11,5 +11,9 @@ window.addEventListener('load', function () {
         events_image.setAttribute('src', './images/homepage/special-events-desktop.jpg');
         /*h2, p*/
     };
+    button_social.onclick=function () {
+        events_image.setAttribute('src', './images/homepage/social-events-desktop.jpg');
+
+
     /*event handler*/
 });
