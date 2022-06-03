ee3d9b2ff3c3204fb3594b777bdad9db9f141de5
alexisshy <30672309+alexisshy@users.noreply.github.com>
2022-06-03 18:24:10 -0400

    add onclick for h2 and p

--- main.js
+++ main.js
@@ -8,15 +8,25 @@ window.addEventListener('load', function () {
 
     button_family.onclick=function () {
         events_image.setAttribute('src', './images/homepage/family-gathering-desktop.jpg');
+        events_title.innerHTML = "Family Gathering";
+        events_text.innerHTML = `We love catering for entire families. So please bring everyone along for a special meal with your 
+        loved ones. We’ll provide a memorable experience for all.`;
     };
     button_special.onclick=function () {
         events_image.setAttribute('src', './images/homepage/special-events-desktop.jpg');
+        events_title.innerHTML = "Special Events";
+        events_text.innerHTML = `Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you. 
+        We’ll be sure to mark your special date with an unforgettable meal.`;
         /*h2, p*/
     };
     button_social.onclick=function () {
         events_image.setAttribute('src', './images/homepage/social-events-desktop.jpg');
+        events_title.innerHTML = "Social Events";
+        events_text.innerHTML = `Are you looking to have a larger social event? No problem! We’re more than happy to cater for big 
+        parties. We’ll work with you to make your event a hit with everyone.`;
         
     };    
+    
 
     /*event handler*/
 });
